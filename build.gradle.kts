4016903f2b657f681b890bd97503e695b5357954
Jake Mathews <jake.mathews.email@gmail.com>
2019-06-07 11:17:20 -0400

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.31"
+}
+
+group = "com.synopsys.integration"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
