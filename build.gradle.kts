1ae36decf584c5c840cfbea2db9a3ebfc289ec0a
Jake Mathews <jake.mathews.email@gmail.com>
2019-06-07 12:42:25 -0400

    build(dependencies) Added common-gradle-plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        jcenter()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath("com.blackducksoftware.integration:common-gradle-plugin:0.0.+")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.3.31"
 }
 
 group = "com.synopsys.integration"
 version = "0.0.1-SNAPSHOT"
+
+apply(plugin = "com.blackducksoftware.integration.simple")
 
 repositories {
     jcenter()
