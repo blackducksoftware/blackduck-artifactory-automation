acd8472c72cecacd726e19df6daa3acaaa083c53
Jake Mathews <jake.mathews.email@gmail.com>
2019-06-13 14:35:04 -0400

    feat(config) Added BlackDuck credentials to config.

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,6 @@
 import artifactory.ArtifactoryUser
 import artifactory.SystemApiService
+import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfigBuilder
 import com.synopsys.integration.log.IntLogger
 import com.synopsys.integration.log.Slf4jIntLogger
 import org.apache.commons.io.IOUtils
@@ -21,7 +22,11 @@ fun main() {
         configManager.getOrDefault(Config.ARTIFACTORY_USERNAME, "admin"),
         configManager.getOrDefault(Config.ARTIFACTORY_PASSWORD, "password"),
         configManager.getOrDefault(Config.ARTIFACTORY_VERSION, "latest"),
-        configManager.getOrThrow(Config.ARTIFACTORY_LICENSE_PATH)
+        configManager.getOrThrow(Config.ARTIFACTORY_LICENSE_PATH),
+        configManager.getOrThrow(Config.BLACKDUCK_URL),
+        configManager.getOrDefault(Config.BLACKDUCK_USERNAME, "sysadmin"),
+        configManager.getOrDefault(Config.BLACKDUCK_PASSWORD, "blackduck"),
+        configManager.getOrDefault(Config.BLACKDUCK_TRUST_CERT, "true")
     )
 }
 
@@ -32,16 +37,28 @@ class Application(
     private val artifactoryUsername: String,
     private val artifactoryPassword: String,
     private val artifactoryVersion: String,
-    private val artifactoryLicensePath: String
+    private val artifactoryLicensePath: String,
+    private val blackduckUrl: String,
+    private val blackDuckUsername: String,
+    private val blackDuckPassword: String,
+    private val blackDuckTrustCert: String
 ) {
     private val logger: IntLogger = Slf4jIntLogger(LoggerFactory.getLogger(this.javaClass))
 
     init {
-        logger.info("Loading Artifactory License")
+        logger.info("Building BlackDuckServerConfig.")
+        val blackDuckServerConfig = BlackDuckServerConfigBuilder()
+            .setUrl(blackduckUrl)
+            .setUsername(blackDuckUsername)
+            .setPassword(blackDuckPassword)
+            .setTrustCert(blackDuckTrustCert.toBoolean())
+            .build()
+
+        logger.info("Loading Artifactory License.")
         val artifactoryLicenseFile = File(artifactoryLicensePath)
         val licenseText = FileInputStream(artifactoryLicenseFile).convertToString().replace("\n", "").replace(" ", "")
 
-        logger.info("Installing and starting artifactory version: $artifactoryVersion")
+        logger.info("Installing and starting Artifactory version: $artifactoryVersion")
         val containerHash = dockerService.installAndStartArtifactory(artifactoryVersion, artifactoryPort)
         logger.info("Artifactory container: $containerHash")
 
